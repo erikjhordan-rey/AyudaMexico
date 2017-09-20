5df2939142a6cbdd4d54bad765eaca0a49f563ac
ErikJhordan <erik.gonzalez@schibsted.com.mx>
2017-09-20 10:03:58 -0500

    fixed merge conflicts

--- app/src/main/java/io/github/erikcaffrey/ayudamexico/announcement/AnnouncementFragment.java
+++ app/src/main/java/io/github/erikcaffrey/ayudamexico/announcement/AnnouncementFragment.java
@@ -1,5 +1,8 @@
 package io.github.erikcaffrey.ayudamexico.announcement;
 
+import android.content.Intent;
+import android.net.Uri;
+import butterknife.OnClick;
 import io.github.erikcaffrey.ayudamexico.R;
 import io.github.erikcaffrey.ayudamexico.common.CoreFragment;
 
@@ -12,34 +15,28 @@ public class AnnouncementFragment extends CoreFragment {
     @Override protected int getLayoutResId() {
         return R.layout.announcement_fragment;
     }
-    
-    
-    @OnClick(R.id.etn_url)
-    public void etn_url(){
+
+    @OnClick({ R.id.etn_url, R.id.card_etn_url }) public void etn_url() {
         openLink("https://goo.gl/HfH3Qv");
     }
 
-    @OnClick(R.id.anima_url)
-    public void anima_url(){
+    @OnClick({ R.id.anima_url, R.id.card_anima_url }) public void anima_url() {
         openLink("https://goo.gl/Vj7xah");
     }
 
-    @OnClick(R.id.telcel_url)
-    public void telcel_url(){
+    @OnClick({ R.id.telcel_url, R.id.card_telcel_url }) public void telcel_url() {
         openLink("https://goo.gl/a5wHEf");
     }
 
-    @OnClick(R.id.startup_url)
-    public void startup_url(){
+    @OnClick({ R.id.startup_url, R.id.card_startup_url }) public void startup_url() {
         openLink("https://goo.gl/P54YT7");
     }
 
-    @OnClick(R.id.hospital_url)
-    public void hospital_url(){
+    @OnClick({ R.id.hospital_url, R.id.card_makin_url }) public void hospital_url() {
         openLink("https://goo.gl/18c35s");
     }
 
-    public void openLink(String url){
+    public void openLink(String url) {
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(url));
         startActivity(i);
