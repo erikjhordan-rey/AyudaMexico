a4033542aac02b1a54275bbe8433423b919e686e
ErikJhordan <erik.gonzalez@schibsted.com.mx>
2017-09-21 13:33:00 -0500

    fixed bugs

--- app/src/main/java/io/github/erikcaffrey/ayudamexico/help/ui/HelpFragment.java
+++ app/src/main/java/io/github/erikcaffrey/ayudamexico/help/ui/HelpFragment.java
@@ -2,12 +2,11 @@ package io.github.erikcaffrey.ayudamexico.help.ui;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Toast;
-
-import java.util.List;
-
 import butterknife.BindView;
 import io.github.erikcaffrey.ayudamexico.R;
 import io.github.erikcaffrey.ayudamexico.common.CoreFragment;
@@ -16,24 +15,27 @@ import io.github.erikcaffrey.ayudamexico.help.model.HelpClient;
 import io.github.erikcaffrey.ayudamexico.help.model.HelpInteractor;
 import io.github.erikcaffrey.ayudamexico.help.presenter.HelpPresenter;
 import io.github.erikcaffrey.ayudamexico.help.ui.adapter.HelpAdapter;
+import java.util.List;
 
 public class HelpFragment extends CoreFragment implements HelpPresenter.Ui {
 
-    @BindView(R.id.recycler_help)
-    RecyclerView recycler_help;
+    @BindView(R.id.recycler_help) RecyclerView recycler_help;
 
     public static HelpFragment newInstance() {
         return new HelpFragment();
     }
+
     private HelpAdapter adapter;
-    private HelpPresenter helpPresenter;
-    private GridLayoutManager lLayout;
-
-
     private HelpPresenter helpPresenter;
 
     @Override protected int getLayoutResId() {
         return R.layout.help_fragment;
+    }
+
+    @Override protected void initFragment(@NonNull View view) {
+        super.initFragment(view);
+        initializeAdapter();
+        initializeRecyclerView();
     }
 
     @Override protected void initPresenter() {
@@ -43,25 +45,20 @@ public class HelpFragment extends CoreFragment implements HelpPresenter.Ui {
         helpPresenter = new HelpPresenter(helpInteractor);
         helpPresenter.setUi(this);
         helpPresenter.loadHelpList();
-        recycler_help = (RecyclerView) this.getView().findViewById(R.id.recycler_help);
-        initializeAdapter();
-        initializeRecyclerView();
     }
 
     private void initializeAdapter() {
         adapter = new HelpAdapter(helpPresenter);
     }
 
-
     private void initializeRecyclerView() {
-        lLayout = new GridLayoutManager(getActivity(), 1);
+        GridLayoutManager lLayout = new GridLayoutManager(getActivity(), 1);
         recycler_help.setLayoutManager(lLayout);
         recycler_help.setAdapter(adapter);
-        //recycler_gallery.invalidate();
     }
 
     @Override public void showHelpList(List<Help> helpList) {
-            adapter.setHelpList(helpList);
+        adapter.setHelpList(helpList);
     }
 
     @Override public void showEmptyMessage() {
@@ -72,8 +69,7 @@ public class HelpFragment extends CoreFragment implements HelpPresenter.Ui {
         Toast.makeText(getContext(), "Ha Ocurrido un error", Toast.LENGTH_LONG).show();
     }
 
-    @Override
-    public void showDetails(Help help) {
+    @Override public void showDetails(Help help) {
         Intent i = new Intent(Intent.ACTION_VIEW);
         i.setData(Uri.parse(help.getLinkMaps()));
         startActivity(i);
