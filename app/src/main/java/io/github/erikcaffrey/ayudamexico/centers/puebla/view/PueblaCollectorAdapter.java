73dd18e1eb54446ea3c37a230bf3b3d0068b5cf7
ErikJhordan <erik.gonzalez@schibsted.com.mx>
2017-09-24 23:28:25 -0500

    added swipe refresh on Puebla fragment

--- app/src/main/java/io/github/erikcaffrey/ayudamexico/centers/puebla/view/PueblaCollectorAdapter.java
+++ app/src/main/java/io/github/erikcaffrey/ayudamexico/centers/puebla/view/PueblaCollectorAdapter.java
@@ -4,38 +4,50 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import io.github.erikcaffrey.ayudamexico.R;
 import io.github.erikcaffrey.ayudamexico.centers.puebla.model.CollectorCenter;
+import io.github.erikcaffrey.ayudamexico.util.StringUtils;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Armando on 24/9/2017.
  */
 
-public class PueblaCollectorAdapter extends RecyclerView.Adapter<PueblaCollectorViewHolder>{
+public class PueblaCollectorAdapter extends RecyclerView.Adapter<PueblaCollectorViewHolder> {
 
     private List<CollectorCenter> centerList = new ArrayList<>();
 
-    @Override
-    public PueblaCollectorViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    @Override public PueblaCollectorViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.centers_item, parent, false);
         return new PueblaCollectorViewHolder(view);
     }
 
-    @Override
-    public void onBindViewHolder(PueblaCollectorViewHolder holder, int position) {
-        holder.bind(centerList.get(position));
+    @Override public void onBindViewHolder(PueblaCollectorViewHolder holder, int position) {
+        final CollectorCenter center = centerList.get(position);
+        holder.bind(center);
     }
 
-    @Override
-    public int getItemCount() {
+    @Override public int getItemCount() {
         return centerList.size();
     }
 
-    public void setCenterList(List<CollectorCenter> centerList) {
-        this.centerList = centerList;
+    void setCenterList(List<CollectorCenter> centerList) {
+        clear();
+        for (CollectorCenter collectorCenter : centerList) {
+            if (isAValidCenter(collectorCenter)) {
+                this.centerList.add(collectorCenter);
+            }
+        }
+    }
+
+    private boolean isAValidCenter(CollectorCenter center) {
+        return !StringUtils.isNullOrEmpty(center.getAsking());
+    }
+
+    private void clear() {
+        if (!this.centerList.isEmpty()) {
+            this.centerList.clear();
+        }
     }
 }
